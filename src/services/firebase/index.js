8f9b519b89d5b936b809fbdbfbb87e64abfd30da
GabrielGarcia <a3522110231@alumno.uttehuacan.edu.mx>
2023-07-20 21:04:26 -0600

    First commit

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,10 +1,10 @@
-const { initializeApp, cert, database } = require("firebase-admin");
+const { initializeApp, cert} = require("firebase-admin");
 
-const serviceAccount = require("../../utils/constanst/firebase-admin.json.json");
+const serviceAccount = require("../../utils/constanst/firebase-admin.json");
 
 const app = initializeApp({
   credential: cert(serviceAccount),
-    databaseURL: "https://pagdcts.firebaseio.com"
+  databaseURL: "https://dsm-3b-gabriel.firebaseio.com"
 });
 
 module.exports = app;
